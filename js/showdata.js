d00341adea7008c28850deee0a9843972bda251a
Aswinkumaresan <aswinabi2000@gmail.com>
2023-02-27 15:21:24 +0530

    update add and quantity

--- js/showdata.js
+++ js/showdata.js
@@ -1,8 +1,6 @@
-
-
 var obj = [
   {
-    id:1,
+    id: 1,
     image: "pro1.jpg",
     productName: "Organic Virgin Cocunet",
     price: 50,
@@ -39,7 +37,7 @@ var obj = [
     productName: "Ambrosia Apples",
     price: 25.39,
     icon: "fa-lock",
-    quantity:0
+    quantity: 0,
   },
   {
     id: 6,
@@ -47,7 +45,7 @@ var obj = [
     productName: "Vegetables Basket",
     price: 11.39,
     icon: "fa-lock",
-    quantity:0
+    quantity: 0,
   },
   {
     id: 7,
@@ -55,15 +53,15 @@ var obj = [
     productName: "Orange Navel Large",
     price: 140,
     icon: "fa-lock",
-    quantity:0
+    quantity: 0,
   },
   {
     id: 8,
     image: "pro8.jpg",
     productName: "Tropicana Orange Juice",
-    price:142,
+    price: 142,
     icon: "fa-lock",
-    quantity:0
+    quantity: 0,
   },
 ];
 
@@ -74,8 +72,6 @@ let itemBox = document.getElementById("item-box");
 function showdata(data) {
   itemBox.innerHTML = "";
   data.forEach(function (elm, i, arr) {
- 
-
     // <----------create Element------------>
 
     let colElm = document.createElement("div");
@@ -84,7 +80,7 @@ function showdata(data) {
     let productCOntentDiv = document.createElement("div");
     let linkedname = document.createElement("a");
     let productName = document.createElement("h3");
-  
+
     let productPrice = document.createElement("h5");
     let linkicon = document.createElement("button");
     let addKart = document.createElement("i");
@@ -95,17 +91,16 @@ function showdata(data) {
     subcolElm.setAttribute("class", "products_list");
     productCOntentDiv.setAttribute("class", "product_listcontent");
     imageElm.setAttribute("src", "assests/products/" + elm.image);
-    linkicon.setAttribute("class","iconbtn");
+    linkicon.setAttribute("class", "iconbtn");
     addKart.setAttribute("class", "fa " + elm.icon);
-   
-    addKart.style.color
+
+    addKart.style.color;
 
     // <------------get value-------->
-   
+
     productName.innerText = elm.productName;
     productPrice.innerText = elm.price;
 
-    
     linkicon.onclick = addCartItem.bind(null, elm.id);
     linkedname.appendChild(productName);
     linkicon.appendChild(addKart);
@@ -120,26 +115,22 @@ showdata(obj);
 
 let linkidicon = document.getElementById("iconidbtn");
 
-function addCartItem(id){
-  
-  let findData =  cartItem.find(function(item){
+function addCartItem(id) {
+  let findData = cartItem.find(function (item) {
     return item.id === id;
-  })
-  if(findData){
-    cartItem.map(function(item){
-      if(item.id === id){
+  });
+  if (findData) {
+    cartItem.map(function (item) {
+      if (item.id === id) {
         item.quantity += 1;
         return item;
-
-      }else
-      return item;
-    })
+      } else return item;
+    });
     console.log(cartItem);
-  }else{
-    let newItemCart = obj.find(function(item){
+  } else {
+    let newItemCart = obj.find(function (item) {
       return item.id === id;
-      
-    })
+    });
     newItemCart.quantity = 1;
     cartItem.push(newItemCart);
     console.log(cartItem);
@@ -147,76 +138,137 @@ function addCartItem(id){
   }
 }
 
-
-let addKartShowData= document.getElementById("addshowcart");
+let addKartShowData = document.getElementById("addshowcart");
 
 function updatekartShow(newdata) {
   addKartShowData.innerHTML = "";
-  newdata.forEach(function (elm, i, arr){
-    
-    let showKartcompletedata=document.createElement("div");
-    let showKartImagedata=document.createElement("div");
-    let showKartdata=document.createElement("div");
-    let karproimage=document.createElement("img");
-    let  kartProNme=document.createElement("h3");
-    let  kartProqty=document.createElement("h5");
-    let  kartProPrice=document.createElement("h4");
+  newdata.forEach(function (elm, i, arr) {
+    let overalldiv = document.createElement("div");
+    let showKartcompletedata = document.createElement("div");
+
+    let showKartImagedata = document.createElement("div");
+    let showKartdata = document.createElement("div");
+    let karproimage = document.createElement("img");
+    let kartProNme = document.createElement("h3");
+    let kartProqty = document.createElement("h5");
+    let kartProPrice = document.createElement("h4");
+    let totalPrice = document.createElement("h4");
+    let buybtn = document.createElement("button");
+    let deletebtn = document.createElement("button");
+
+    // kart add and sub
+    let itemqtyadd = document.createElement("button");
+    // let qtydisplay=document.createElement("input")
+    let itemqtsub = document.createElement("button");
+
+    overalldiv.setAttribute("class", "overalldiv");
+    showKartcompletedata.setAttribute("class", "showKartcompletedata");
+    showKartImagedata.setAttribute("class", "showKartImagedata");
+    karproimage.setAttribute("class", "dataimage");
+    showKartdata.setAttribute("class", "showContentData");
+    itemqtyadd.setAttribute("class", "btnitemadd");
+    itemqtsub.setAttribute("class", "btnitemadd");
+    buybtn.setAttribute("class", "btn btn-danger");
+    deletebtn.setAttribute("class", "btn btn-danger");
+    // qtydisplay.setAttribute("id","qtyshow")
+    // qtydisplay.setAttribute("value",0)
+
+    // buybtn.style.textAlign("center");
+
+    totalvalue = elm.quantity * elm.price;
+    totalPrice.innerText = totalvalue;
+    // console.log(totalPrice);
+    // console.log(totalvalue);
+
+    kartProNme.innerText = "Product Name:  " + elm.productName;
+    kartProPrice.innerText = "Product Price: " + elm.price;
+    kartProqty.innerText = "Product Quantity:  " + elm.quantity;
+    totalPrice.innerText = "Total Price :" + totalvalue;
+    itemqtyadd.innerText = " + ";
+    itemqtsub.innerText = " - ";
+    buybtn.innerText = "Buy Items";
+    deletebtn.innerText = "Delete";
+    // qtydisplay.innerText=elm.quantity;
+
+    karproimage.setAttribute("src", "assests/products/" + elm.image);
+
+    showKartdata.append(
+      kartProNme,
+      kartProqty,
+      kartProPrice,
+      totalPrice,
+      itemqtyadd,
+      itemqtsub,
+      deletebtn
+    );
+    showKartImagedata.appendChild(karproimage);
+    showKartcompletedata.append(showKartImagedata, showKartdata);
+    addKartShowData.append(showKartcompletedata);
 
 
-    let itemqtyadd=document.createElement("button");
-    let qtydisplay=document.createElement("input")
-    let itemqtsub=document.createElement("button");
-  
+    // addKartShowData.appendChild(showKartcompletedata);
+    // overalldiv.appendChild(buybtn);
+    // overalldiv.append(buybtn);
 
-    showKartcompletedata.setAttribute("class","showKartcompletedata");
-    karproimage.setAttribute("class","dataimage");
-    showKartdata.setAttribute("class","showContentData");
-    itemqtyadd.setAttribute("class","btnitemadd");
-    itemqtsub.setAttribute("class","btnitemadd");
-    qtydisplay.setAttribute("id","qtyshow")
-    qtydisplay.setAttribute("value",0)
+    // addKartShowData.appendChild(buybtn);
+
+    // itemqtyadd.addEventListener("click",()=>{
+    //   qtydisplay.value=parseInt(qtydisplay.value) + 1;
+    //   console.log(qtydisplay);
+    // })
+
+    // itemqtsub.addEventListener("click",()=>{
+    //   if(qtydisplay.value<=0){
+    //     qtydisplay.value=0;
+    //   }
+    //   else{
+    //     qtydisplay.value=parseInt(qtydisplay.value) - 1;
+
+    //   }
+    //   // console.log(qtydisplay);
+    // })
+
+    let quantity = 1;
+
+    itemqtyadd.addEventListener("click", () => {
+      quantity++;
+      kartProqty.innerText = "Product Quantity:  " + quantity;
+      // console.log(kartProqty);
+      kartProPrice.innerText = "Product Price: " + elm.price;
+      // console.log(kartProPrice);
+      totalPrice.innerText = "Total price :" + quantity * elm.price;
+      // console.log(totalPrice  )
+    });
+
+    itemqtsub.addEventListener("click", () => {
+      if (quantity > 0) {
+        quantity--;
+      } else {
+        return;
+      }
+
+      kartProqty.innerText = "Product Quantity:  " + quantity;
+      kartProPrice.innerText = "Product Price: " + elm.price;
+      // console.log(kartProPrice);
+      totalPrice.innerText = "Total price :" + quantity * elm.price;
+      // console.log(totalPrice  )
+    });
 
 
-        kartProNme.innerText="Product Name:  "+ elm.productName;
-        kartProPrice.innerText="Product Price: "+ elm.price;
-        kartProqty.innerText="Product Quantity:  "+ elm.quantity;
-        itemqtyadd.innerText=" + ";
-        itemqtsub.innerText=" - ";
-        // qtydisplay.innerText=elm.quantity;
+    // deletebtn.onclick = deletedata.bind(null, elm.id);
+    // window.onload = update(cartItem);
 
-
-        karproimage.setAttribute("src","assests/products/"+ elm.image)
-        
-
-        showKartdata.append(kartProNme,kartProqty,kartProPrice,itemqtyadd,qtydisplay,itemqtsub);
-        showKartImagedata.appendChild(karproimage)
-        showKartcompletedata.append(showKartImagedata,showKartdata)
-        addKartShowData.appendChild(showKartcompletedata);
-  
-
-        itemqtyadd.addEventListener("click",()=>{
-          qtydisplay.value=parseInt(qtydisplay.value) + 1;
-          console.log(qtydisplay);
-        })
-
-        itemqtsub.addEventListener("click",()=>{
-          if(qtydisplay.value<=0){
-            qtydisplay.value=0;
-          }
-          else{
-            qtydisplay.value=parseInt(qtydisplay.value) - 1;
-
-          }
-          // console.log(qtydisplay);
-        })
-  
-
-  })
+    // function deletedata(id) {
+    //   let finddata = cartItem.find(function (items) {
+    //     return items.id === id;
+    //   });
+    //   if (finddata) {
+    //     let updateobject = cartItem.filter(function (items) {
+    //       return items.id !== id;
+    //     });
+    //     cartItem = updateobject;
+    //     update(cartItem);
+    //   }
+    // }
+  });
 }
-
-
-
-
-
-
-
